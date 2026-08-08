005c5d104a7da113b2c98619ac011ff66782113f
Andrei <andrey_filipoi@yahoo.com>
2026-08-08 19:52:07 +0100

    Added journeyType and WeeklyMileage dropdown when adding a car for better maintenance estimate

--- src/AddCarPanel.js
+++ src/AddCarPanel.js
@@ -6,6 +6,8 @@ function AddCarPanel({ onClose, onSave }) {
   const [carModel, setCarModel] = useState('');
   const [carYear, setCarYear] = useState('');
   const [carMileage, setCarMileage] = useState('');
+  const [journeyType, setJourneyType] = useState('mixed');
+  const [weeklyMileage, setWeeklyMileage] = useState('');
 
   function handleSave(){
     const newCar = {
@@ -14,7 +16,10 @@ function AddCarPanel({ onClose, onSave }) {
       model: carModel,
       year: carYear,
       mileage: Number(carMileage),
+      journeyType: journeyType,
+      weeklyMileage: weeklyMileage
     };
+    console.log(newCar); 
     onSave(newCar);
   };
     return(
@@ -33,6 +38,23 @@ function AddCarPanel({ onClose, onSave }) {
 
         <label> Mileage </label>   
         <input value = {carMileage} onChange = {(e) => setCarMileage(e.target.value)}/>
+
+        <label> Journey Type</label>
+        <select value = {journeyType} onChange = {(e) => setJourneyType(e.target.value)}>
+            <option value = "city"> Mostly City</option>
+            <option value = "highway"> Mostly Highway</option>
+            <option value = "mixed">Mixed City and Highway</option>
+        </select>
+        <label> Weekly Mileage </label>
+        <select value = {weeklyMileage} onChange = {(e) => setWeeklyMileage(e.target.value)}>
+            <option value = "25"> under 50 mi</option>
+            <option value = "75"> 50-100 mi </option>
+            <option value = "150"> 100-200 mi </option>
+            <option value = "250"> 200-300 mi </option>
+            <option value = "350"> 300-400 mi</option>
+            <option value = "450"> over 400 mi </option>
+
+        </select>
 
         <div className = "modal-actions"> 
             <button onClick = {onClose}> Cancel </button>
